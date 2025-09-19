0fda4bad36170f6602d701a9e66163097666e01c
a-burba <a.burba@wustl.edu>
2025-09-19 13:56:38 -0500

    material density constants added

---
+++ src/references/materialDensity.h
@@ -0,0 +1,13 @@
+#pragma once
+// MATERIAL DENSITY REFERENCE TABLE
+// Critical Data - Use these precise densities for ALL calculations
+
+//          Material	 |   Density (LB/SQIN)
+const float ACOUSTIC_PANEL_DENSITY = 0.0038;
+const float ACOUSTIC_PANEL_FRAMED_DENSITY =	0.0037;
+const float ACRYLIC_DENSITY = 0.0094;
+const float CANVAS_FRAMED_DENSITY  = 0.0085;
+const float CANVAS_GALLERY_DENSITY = 0.0061;
+const float GLASS_DENSITY   = 0.0098;
+const float MIRROR_DENSITY  = 0.0191;
+const float PATIENT_BOARD_DENSITY  = 0.0347;
