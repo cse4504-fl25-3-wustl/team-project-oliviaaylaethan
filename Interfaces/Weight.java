c19b601bfdca00fc7fc925eb7d1e09732dd67a1e
OGuess10 <66238560+OGuess10@users.noreply.github.com>
2025-09-19 11:50:18 -0500

    Adds basic weight class interface (INCOMPLETE)

---
+++ Interfaces/Weight.java
@@ -0,0 +1,15 @@
+interface Weight {
+    // variables
+    // private float itemWeights [];
+    // private float boxWeights [];
+    // private float palletWeights [];
+    // private float totalWeight;
+
+    // methods
+    // item weights
+    // public getItemWeight(item)
+
+    // final weights
+    // public getAllItemWeights(itemWeights) -> total item weight
+    // public getFinalWeight() -> return totalWeight
+    //
