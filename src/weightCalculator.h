c3d4d48cb759f061df93665e010d88a468abcff5
OGuess10 <66238560+OGuess10@users.noreply.github.com>
2025-09-24 11:56:16 -0500

    Adds weight calculator header file

--- src/weightCalculator.h
+++ src/weightCalculator.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <list>
+#include "item.h"
+
+using std::list;
+
+class WeightCalculator {
+    private:
+        float calculateItemWeight(Item item);
+        float calculateTotalPalletWeight(int standard, int oversized, int crates);
+
+    public:
+        WeightCalculator() {}
+        float calculateTotalWeight(list<Item> items, int standard, int oversized, int crates);
+};
