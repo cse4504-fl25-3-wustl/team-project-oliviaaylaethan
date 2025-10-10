0c15f6f3aced4e7281a2e3af22eee24bb0aef2d2
a-burba <a.burba@wustl.edu>
2025-10-10 01:06:58 -0500

    Combined Crate and Pallet classes into ShippingContainer class (limited file commits to hopefully limit merge conflicts)

--- src/entities/shippingContainer.h
+++ src/entities/shippingContainer.h
@@ -2,23 +2,68 @@
 #include "box.h"
 #include <vector>
 
-// Abstract base class for any shipping container (Pallet or Crate)
+// Crate info
+const Dimensions STANDARD_CRATE_DIMENSIONS = {50, 38, 0};
+const float STANDARD_CRATE_TARE_WEIGHT = 125;
+const int TEMP_CRATE_OVERSIZED_BOX_CAPACITY = 3; // TODO determine real values later
+const int TEMP_CRATE_STANDARD_BOX_CAPACITY = 4; // TODO determine real values later
+
+
+// Pallet info
+// For standard shipments
+const Dimensions STANDARD_PALLET_DIMENSIONS = {48, 40, 0};
+const float STANDARD_PALLET_TARE_WEIGHT = 60;
+const int STANDARD_PALLET_STANDARD_BOX_CAPACITY = 4;
+
+// For small shipments
+const Dimensions GLASS_SMALL_PALLET_DIMENSIONS = {43, 35, 0};
+const float GLASS_SMALL_PALLET_TARE_WEIGHT = 60;
+const int GLASS_SMALL_PALLET_STANDARD_BOX_CAPACITY = 0;
+
+// Oversize pallet: 5 standard boxes
+const Dimensions OVERSIZE_PALLET_DIMENSIONS = {60, 40, 0};
+const float OVERSIZE_PALLET_TARE_WEIGHT = 75;
+const int OVERSIZE_PALLET_STANDARD_BOX_CAPACITY = 5;
+
+const int TEMP_PALLET_OVERSIZED_BOX_CAPACITY = 3; // TODO determine real values later
+
+enum ShippingContainerType {
+    STANDARD_PALLET,
+    GLASS_SMALL_PALLET,
+    OVERSIZE_PALLET,
+    STANDARD_CRATE,
+    OTHER
+};
+
+
+
+// class for any shipping container (Pallet or Crate)
 class ShippingContainer {
-protected:
+private:
     Dimensions dimensions_;
     float tareWeight_; // Weight of the empty container
     std::vector<Box> contents_;
+    ShippingContainerType shippingContainerType_;
 
+    int standardBoxCapacity_;
+    int oversizedBoxCapacity_; 
+    
 public:
-    virtual ~ShippingContainer() = default;
+    ShippingContainer();
+    ShippingContainer(Dimensions dimensions, float tareWeight, int standardBoxCapacity, int oversizedBoxCapacity, ShippingContainerType shippingContainerType);
+    
+    static ShippingContainer makeStandardPallet();
+    static ShippingContainer makeGlassSmallPallet(); // TODO: What is glass small pallet?
+    static ShippingContainer makeOversizePallet();
+    static ShippingContainer makeStandardCrate();
 
-    // Constructor defined so that pallet and crate can set dimensions and tareWeight
-    ShippingContainer(Dimensions dimensions, float tareWeight)
-        : dimensions_(dimensions), tareWeight_(tareWeight) {}
+    float calculateContainerHeight();
 
-    // Pure virtual methods that must be implemented by derived classes
-    virtual Dimensions getDimensions() const = 0;
-    virtual float getTareWeight() const = 0;
-    virtual std::vector<Box> getContents() const = 0;
-    virtual void addBox(Box box) = 0;
+    Dimensions getDimensions() const;
+    float getTareWeight() const;
+    std::vector<Box> getContents() const;
+    ShippingContainerType getShippingContainerType() const;
+    int getStandardBoxCapacity() const;
+    int getOversizedBoxCapacity() const;
+    void addBox(Box box);
 };
