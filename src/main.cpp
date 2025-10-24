db501952bf9cd8534c053f2be3e86173f9fb178d
OGuess10 <66238560+OGuess10@users.noreply.github.com>
2025-10-24 11:58:28 -0500

    Adds command line arguments instead of previous cli

--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,26 @@
 #include "responses/response.h"
 #include <iostream>
 
-int main() {
-    std::string dataInputFile;
-    std::string requirementsInputFile;
-    bool run = true;
-    int choice;
-
-    std::cout << "\n---------- ART PACKER -----------" << std::endl;
-
-    while (run) {
-        std::cout << "\nWelcome to Art Packer." << std::endl;
-        std::cout << "(1) Pack art\n(2) Quit" << std::endl;
-        std::cout << "Please select an option: ";
-        std::cin >> choice;
-
-        if (std::cin.fail()) {
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Invalid input. Please enter 1 or 2.\n";
-            continue;
-        }
-
-        if (choice == 1) {
-            std::cout << "Enter data input file: ";
-            std::cin >> dataInputFile;
-            std::cout << "\nEnter site requirements file: ";
-            std::cin >> requirementsInputFile;
-            std::cout << std::endl;
-
-            CsvParser parser;
-            if (!parser.isValidFile(dataInputFile) || !parser.isValidFile(requirementsInputFile)) {
-                continue;
-            }
-            Request request = parser.parseFiles(dataInputFile, requirementsInputFile);
-            PackingInteractor packingInteractor;
-            Response response = packingInteractor.packAllArt(request);
-            response.printPackingSummary();
-            response.printWeightSummary();
-            response.printBusinessIntelSummary();
-            
-            continue;
-        }
-        else if (choice == 2) {
-            run = false;
-            break;
-        }
-        else {
-            std::cout << "Please select 1 or 2." << std::endl;
-            continue;
-        }
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        std::cout << "Usage: " << argv[0] << " <art_data_file.csv> <requirements_file.csv>" << std::endl;
+        return 1;
     }
+    std::string dataInputFile = argv[1];
+    std::string requirementsInputFile = argv[2];
+
+    CsvParser parser;
+    if (!parser.isValidFile(dataInputFile) || !parser.isValidFile(requirementsInputFile)) {
+        std::cout << "Error: One or both input files are invalid." << std::endl;
+        return 1;
+    }
+    
+    Request request = parser.parseFiles(dataInputFile, requirementsInputFile);
+    PackingInteractor packingInteractor;
+    Response response = packingInteractor.packAllArt(request);
+    response.printPackingSummary();
+    response.printWeightSummary();
+    response.printBusinessIntelSummary();
 
     return 0;
 }
